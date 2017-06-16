f7b0469ee9526b1fc2e5fd35a116225d8be05f2c
Dom Williams <me@domwillia.ms>
2017-06-16 12:15:08 +0200

    Add args and config parsing

--- scranagement.py
+++ scranagement.py
@@ -5,7 +5,10 @@ import glob
 import sys
 from collections import namedtuple
 
-Config = namedtuple('Config', ['directory', 'latest_n'])
+import configargparse
+import configparser
+
+Config = namedtuple("Config", ["directory", "latest_n"])
 
 
 def get_n_latest(directory, n):
@@ -33,23 +36,39 @@ def _expand_path(path):
     return os.path.expanduser(os.path.expandvars(path))
 
 
+def debug(msg):
+    print(msg, file=sys.stderr)
+
+
 def parse_args():
-    # TODO parse from args
-    directory = "$HOME/screenshots"
-    n = 3
-    return (True, Config(directory=_expand_path(directory), latest_n=n))
+    default_conf = _expand_path("$XDG_CONFIG_HOME/scranagement.conf")
+    # TODO choose $HOME if $XDG_CONFIG_HOME doesnt exist
 
+    p = configargparse.ArgParser(default_config_files=[default_conf])
 
-def show_usage():
-    print("Usage: X")  # TODO
+    p.add("-c", "--conf", required=False, is_config_file=True)
+    p.add("-n", "--latest", required=False, type=int, default=1)
+    p.add("-d", "--directory", required=True)
+    p.add("--save", required=False, action="store_true")
+
+    opts = vars(p.parse_args())
+
+    # remove unneeded option
+    opts.pop("conf", None)
+
+    # save to config file
+    if opts.pop("save"):
+        cp = configparser.ConfigParser()
+        cp.read_dict({"settings": opts})
+        with open(default_conf, "w") as f:
+            cp.write(f)
+        debug("Wrote config to %s" % default_conf)
+        sys.exit(0)  # TODO return success-and-dont-continue exit code
+
+    return Config(directory=_expand_path(opts["directory"]), latest_n=opts["latest"])
 
 
 if __name__ == "__main__":
-    (success, conf) = parse_args()
-    if not success:
-        show_usage()
-        sys.exit(1)
-
-    res = main(conf)
+    res = main(parse_args())
     exit = 0 if res else 2
     sys.exit(exit)
